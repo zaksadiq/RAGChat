8a15cd803996255c270fb0717cdeb218ff2a14f4
Zak Sadiq <zaksadiq0@gmail.com>
2025-02-03 13:45:03 +0000

    Add working hello world API functionality, on front and backend.

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello world!"
+# Prevent CORS warning/error on frontend. 
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/message")
+def get_message():        
+    return jsonify({ "message": "Hello world!" })
